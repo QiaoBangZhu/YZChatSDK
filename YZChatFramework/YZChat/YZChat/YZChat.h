1fa6464e02aa4728bedecbf4ad81d6522f7cf1f1
ziruozimi <ziruozimi@gmail.com>
2020-12-11 20:42:36 +0800

    add framework

---
+++ YZChatFramework/YZChat/YZChat/YZChat.h
@@ -0,0 +1,18 @@
+//
+//  YZChat.h
+//  YZChat
+//
+//  Created by 安笑 on 2020/12/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YZChat.
+FOUNDATION_EXPORT double YZChatVersionNumber;
+
+//! Project version string for YZChat.
+FOUNDATION_EXPORT const unsigned char YZChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YZChat/PublicHeader.h>
+
+
